27109b9e5026d9d00bdae8e2c7dc5b2b265d46d4
ManojNadar <manojviru03@gmail.com>
2023-08-07 19:11:18 +0530

    node practice

---
+++ server.mjs
@@ -0,0 +1,24 @@
+// import os from "os";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// console.log("HELLO ,WORLD");
+// console.log(global);
+
+// *********************************//
+// Operating System
+
+// console.log(os.type());
+// console.log(os.homedir());
+// console.log(os.version());
+
+// =========================== //
+// All Path Names
+
+// const __filename = fileURLToPath(import.meta.url);
+
+// const __dirname = path.dirname(__filename);
+// const __dirname = path.basename(__filename);
+// const __dirname = path.extname(__filename);
+// const __dirname = path.parse(__filename);
+// console.log(__dirname);
